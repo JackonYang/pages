2d1d01e8a427f959cb9a1f9c4ce0b71e824b1275
Jackon Yang <i@jackon.me>
2016-03-31 07:08:44 +0800

    site name change

--- flush.sh
+++ flush.sh
@@ -1,5 +1,6 @@
 sed -i '' 's/.aspx/.html/g' `grep '.aspx' -rl public/*`
 sed -i '' 's/Images/images/g' `grep 'Images' -rl public/*`
+sed -i '' 's/2479/pgn888/g' `grep '2479' -rl public/*`
 sed -i '' 's/ShowIMG.html/images\/auth_code.jpeg/g' `grep 'ShowIMG' -rl public/*`
 sed -i '' 's/\.\.\/Register.aspx/Register.html/g' `grep 'Register\.aspx' -rl public/*`
 sed -i '' 's/<a href="javascript:;" onclick="ImgButtonOnclick()">/<a href="UsersMembers.html">/g' `grep 'ImgButtonOnclick' -rl public/*`
