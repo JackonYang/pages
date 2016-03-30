a5e17ea3a3b164f0af725f2f5f0cc0bd36a48248
Jackon Yang <i@jackon.me>
2016-03-31 03:08:10 +0800

    home page fix

---
+++ public/scan.py
@@ -0,0 +1,20 @@
+# -*- Encoding: utf-8 -*-
+import requests
+import os
+import re
+
+def find_img(css_name):
+    with open(css_name, 'rb') as f:
+        content = ''.join(f.readlines())
+
+    ptn = re.compile(r'url\((.*?)\)')
+    return ptn.findall(content)
+
+pics = []
+
+for f in os.listdir('.'):
+    if f.endswith('.css') or f.endswith('.html'):
+        pics.extend(find_img(f))
+
+for i in set(pics):
+    print i
