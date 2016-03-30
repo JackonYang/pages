7df7512c47b8289319ce4837327a0106f83f2c6b
Jackon Yang <i@jackon.me>
2016-03-30 23:47:39 +0800

    准备用正则解析 css link

--- grab.py
+++ grab.py
@@ -1,12 +1,17 @@
 # -*- Encoding: utf-8 -*-
 import requests
 import os
+import re
 
 
 def download(url, name):
     r = requests.get('http://2479.com/')
     with open(name, 'wb') as f:
         f.write(r.text.encode(r.encoding))
+
+
+def find_css(content):
+    return ['css']
 
 
 if __name__ == '__main__':
@@ -17,4 +22,10 @@ if __name__ == '__main__':
 
     # download home page
     home_name = os.path.join(path, 'index.html')
-    download(url_home, home_name)
+    # download(url_home, home_name)
+
+    with open(home_name, 'rb') as f:
+        c = ''.join(f.readlines())
+
+    links = find_css(c)
+    print links
