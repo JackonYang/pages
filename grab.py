f03d7b585bbba183ec623de5c3f1853d0b663cd1
Jackon Yang <i@jackon.me>
2016-03-30 23:45:08 +0800

    save html in public path

--- grab.py
+++ grab.py
@@ -1,5 +1,6 @@
 # -*- Encoding: utf-8 -*-
 import requests
+import os
 
 
 def download(url, name):
@@ -8,6 +9,12 @@ def download(url, name):
         f.write(r.text.encode(r.encoding))
 
 
-home = 'http://2479.com/'
+if __name__ == '__main__':
+    url_home = 'http://2479.com/'
+    path = 'public'
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-download(home, 'index.html')
+    # download home page
+    home_name = os.path.join(path, 'index.html')
+    download(url_home, home_name)
