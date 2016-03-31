4f7896d7c160e4198804f9fdd25647997ac683f5
Jackon Yang <i@jackon.me>
2016-03-31 10:28:57 +0800

    remove 360

--- flush.sh
+++ flush.sh
@@ -1,6 +1,7 @@
 sed -i '' 's/.aspx/.html/g' `grep '.aspx' -rl public/*`
 sed -i '' 's/Images/images/g' `grep 'Images' -rl public/*`
 sed -i '' 's/夺宝/宝石/g' `grep '夺宝' -rl public/*`
+sed -i '' 's/360网站安全检测平台//g' `grep '360网站安全检测平台' -rl public/*`
 sed -i '' 's/http:\/\/www.2479.com\/upload\/logo/images/g' `grep 'logo' -rl public/*`
 sed -i '' 's/ShowIMG.html/images\/auth_code.jpeg/g' `grep 'ShowIMG' -rl public/*`
 sed -i '' 's/\.\.\/Register.aspx/Register.html/g' `grep 'Register\.aspx' -rl public/*`
